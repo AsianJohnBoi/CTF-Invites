d986d54689f31ab29fcb055cb5189fc8dc09fdb1
John Santias <32500331+AsianJohnBoi@users.noreply.github.com>
2019-03-30 20:49:27 +1000

    Add files via upload

---
+++ Post.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<meta name="description" content="You have to hack your way in!" /> 
+	<meta name="author" content="h3xart - John ;)"> 
+	<meta property="og:title" content="You're invited" /> /> 
+	<title>You're invited</title>
+<!-- 	<script defer src="js/invite.js"></script>
+ -->	<script src="js/functions.js"></script>
+	<link rel="stylesheet" href="css/htb-frontend.css" /> 
+	<link rel="icon" href="images/favicon_32.png"> 
+</head> 
+<body class="blank" style="overflow-y:hidden; "> 
+	<h1>Error 404. Page not found.</h1>
+	<?php
+		echo "data";
+	?>
+</body> 
+</html>
---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function response() {
+	console.log("You're on the right track");
+}
+
+function verifyInviteCode() {
+
+}
+
+function makeInviteCode() {
+	var theData = {
+		data: "SW4gb3JkZXIgdG8gZ2VuZXJhdGUgdGhlIGludml0ZSBjb2RlLCBtYWtlIGEgUE9TVCByZXF1ZXN0IHRvIC9pbnZpdGUvZ2VuZXJhdGU=",
+		enctype: "BASE64"
+	};
+	return theData;
+}
